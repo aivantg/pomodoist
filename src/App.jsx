0fca947ceb7f119d49cd1124050f30569f635aa1
alejandrogarciasalas <garciasalasalejandro@gmail.com>
2019-04-03 16:21:17 -0700

    pomodoro and todolist

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,134 @@
 import React from 'react';
 import Timer from './components/Timer';
-import SessionsCompletedCounter from './components/SessionsCompletedCounter';
+import TodoItem from './components/TodoItem';
+import TodoInput from './components/TodoInput';
 import './styles/App.css';
+import { ReactComponent as EmptyStateIllustration } from './icons/drawkit-nature-man-colour.svg';
+import { ReactComponent as Archive } from './icons/archive.svg';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.addItem = this.addItem.bind(this);
+    this.clearCompletedItems = this.clearCompletedItems.bind(this);
+    this.startSession = this.startSession.bind(this);
     this.increaseSessionsCompleted = this.increaseSessionsCompleted.bind(this);
+    this.toggleItemIsCompleted = this.toggleItemIsCompleted.bind(this);
 
     this.state = {
-      sessionsCompleted: 0,
+      items: [],
+      nextItemId: 1,
     };
   }
 
-  increaseSessionsCompleted() {
-    this.setState(prevState => ({ sessionsCompleted: prevState.sessionsCompleted + 1 }));
+  addItem(description) {
+    const { nextItemId } = this.state;
+    const newItem = {
+      id: nextItemId,
+      description,
+      sessionsCompleted: 0,
+      isCompleted: false,
+    };
+    this.setState((prevState => ({
+      items: prevState.items.concat(newItem),
+      nextItemId: prevState.nextItemId + 1,
+    })));
+  }
+
+  clearCompletedItems() {
+    this.setState(prevState => ({
+      items: prevState.items.filter(item => !item.isCompleted),
+    }));
+  }
+
+  increaseSessionsCompleted(itemId) {
+    this.setState((prevState) => { // TODO: refactor this
+      const newItems = prevState.items.map((item) => {
+        if (item.id === itemId) {
+          item.sessionsCompleted += 1;
+        }
+        return item;
+      });
+
+      return ({ items: newItems });
+    });
+  }
+
+  toggleItemIsCompleted(itemId) {
+    this.setState((prevState) => { // TODO: refactor this.
+      const newItems = prevState.items.map((item) => {
+        if (item.id === itemId) {
+          item.isCompleted = !item.isCompleted;
+        }
+        return item;
+      });
+      return ({ items: newItems });
+    });
+  }
+
+  startSession(id) {
+    this.setState({
+      sessionIsRunning: true,
+      itemIdRunning: id,
+    });
   }
 
   render() {
-    const { sessionsCompleted } = this.state;
-    console.log(sessionsCompleted);
+    const { items, sessionIsRunning, itemIdRunning } = this.state;
     return (
-      <div className="container">
-        <h1 className="header">Today</h1>
-        <Timer mode="WORK" onSessionComplete={this.increaseSessionsCompleted} />
-        <SessionsCompletedCounter sessionsCompleted={sessionsCompleted} />
+      <div className="flex-wrapper">
+        <div className="container">
+          <header>
+            <h1 className="heading">Today</h1>
+            <button className="clear-button" type="button" onClick={this.clearCompletedItems}>
+              <Archive />
+            </button>
+          </header>
+          {sessionIsRunning && (
+            <Timer
+              mode="WORK"
+              onSessionComplete={() => this.increaseSessionsCompleted(itemIdRunning)}
+              isPlaying
+              // Why use key? This is a fully uncontrolled component with a key
+              // https://reactjs.org/blog/2018/06/07/you-probably-dont-need-derived-state.html#recommendation-fully-controlled-component
+              // Timer accepts a isPlaying prop for the initial value of the isPlaying state variable
+              // and then ignores every subsequent change to that prop.
+              key={itemIdRunning}
+            />
+          )}
+          {
+            (items && items.length)
+              ?
+              (
+                <div className="items-container">
+                  {items.map(({
+                    id,
+                    description,
+                    sessionsCompleted,
+                    isCompleted,
+                  }) => (
+                    <TodoItem
+                      key={id}
+                      description={description}
+                      sessionsCompleted={sessionsCompleted}
+                      isCompleted={isCompleted}
+                      startSession={() => this.startSession(id)}
+                      toggleIsCompleted={() => this.toggleItemIsCompleted(id)}
+                    />
+                  ))}
+                </div>
+              )
+              : (
+                <div className="empty-state-container">
+                  <EmptyStateIllustration />
+                  <p className="empty-state-text">Your todolist is empty</p>
+                </div>
+              )
+          }
+        </div>
+        <footer>
+          <TodoInput addItem={this.addItem} />
+        </footer>
       </div>
     );
   }
--- src/components/SessionsCompletedCounter.jsx
+++ src/components/SessionsCompletedCounter.jsx
@@ -4,8 +4,9 @@ import '../styles/sessionsCompletedCounter.css';
 function SessionsCompletedCounter({ sessionsCompleted }) {
   return (
     <div className="sessions-completed-container">
-      <span className="sessions-completed-label">Sessions Completed</span>
-      <span className="sessions-completed-number">{ sessionsCompleted }</span>
+      {/* using index as a key is an anti-pattern but should not matter for this case
+        https://medium.com/@robinpokorny/index-as-a-key-is-an-anti-pattern-e0349aece318 */}
+      {[...Array(sessionsCompleted)].map((_, index) => <div className="sessions-completed-line" key={index} /> )}
     </div>
   );
 }
